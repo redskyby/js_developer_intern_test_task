fc02432f87d2b8fa7a64e5a936d6b0cc09474d1d
Docenko Pasha <pashadocenko@gmail.com>
2024-01-05 23:29:40 +0300

    ClientDetails.tsx was done. JobDetails.tsx was started to init.

--- src/components/jobDetails/JobDetails.tsx
+++ src/components/jobDetails/JobDetails.tsx
@@ -6,6 +6,23 @@ const JobDetails = () => {
             <div className={style.main_title}>
                 <h1>Job details</h1>
             </div>
+            <form>
+                <select id="jobs" name="jobs">
+                    <option value="">Select job type:</option>
+                    <option value="value1">value1</option>
+                    <option value="value2">value2</option>
+                    <option value="value3">value3</option>
+                    <option value="value4">value4</option>
+                </select>
+                <select id="josSource" name="josSource">
+                    <option value="">Select job type:</option>
+                    <option value="value1">value1</option>
+                    <option value="value2">value2</option>
+                    <option value="value3">value3</option>
+                    <option value="value4">value4</option>
+                </select>
+                <textarea id="comment" name="comment"></textarea>
+            </form>
         </div>
     );
 };
